6a7611e2c3325b78992010e8d78de58bc8658bb0
Lucian Maran <lucian.maran@visma.com>
2023-05-23 17:17:17 +0300

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace programa_matematica.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
